54dab39f842aceb8a5846da6bfbdda7e842b1318
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-09-11 11:54:40 -0500

    Rename package

--- blazingdb-calcite-application/src/main/java/com/blazingdb/calcite/application/UnixServer.java
+++ blazingdb-calcite-application/src/main/java/com/blazingdb/calcite/application/UnixServer.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.blazingdb.application;
+package com.blazingdb.calcite.application;
 
 import jnr.enxio.channels.NativeSelectorProvider;
 import java.io.IOException;
