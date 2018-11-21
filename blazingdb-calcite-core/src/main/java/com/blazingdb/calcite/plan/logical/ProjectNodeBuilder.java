b8e8dfb02f2d90b2bc7b2ba994cb9cc69bb858aa
gcca <cristhian@blazingdb.com>
2018-11-21 13:04:36 -0500

    [logical-plan] Validate rex node kind

--- blazingdb-calcite-core/src/main/java/com/blazingdb/calcite/plan/logical/ProjectNodeBuilder.java
+++ blazingdb-calcite-core/src/main/java/com/blazingdb/calcite/plan/logical/ProjectNodeBuilder.java
@@ -3,6 +3,7 @@ package com.blazingdb.calcite.plan.logical;
 import org.apache.calcite.rel.logical.LogicalProject;
 import org.apache.calcite.rex.RexInputRef;
 import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.sql.SqlKind;
 import org.apache.calcite.util.Pair;
 
 import java.util.ArrayList;
@@ -18,17 +19,32 @@ final class ProjectNodeBuilder implements NodeBuilder {
   }
 
   @Override
-  public Node build() {
+  public ProjectNode build() throws NoRexInputRefInLogicalFilterException {
     final ArrayList<String> columnNames   = new ArrayList<String>();
     final ArrayList<Integer> columnValues = new ArrayList<Integer>();
 
     for (Pair<RexNode, String> pair : logicalProject.getNamedProjects()) {
+      final RexNode rexNode = pair.getKey();
+
+      if (!rexNode.isA(SqlKind.INPUT_REF)) {
+        throw new NoRexInputRefInLogicalFilterException("Bad rex node kind: " +
+                                                        rexNode.getKind());
+      }
+
       columnNames.add(pair.getValue());
-      columnValues.add(((RexInputRef)pair.getKey()).getIndex());
+      columnValues.add(((RexInputRef) rexNode).getIndex());
     }
 
     ProjectNode logicalNode = new ProjectNode(columnNames, columnValues);
 
     return logicalNode;
+  }
+
+  final static class NoRexInputRefInLogicalFilterException
+      extends NodeBuilder.BuildingException {
+    private static final long serialVersionUID = 9067377235514275389L;
+    public NoRexInputRefInLogicalFilterException(final String message) {
+      super(message);
+    }
   }
 }
