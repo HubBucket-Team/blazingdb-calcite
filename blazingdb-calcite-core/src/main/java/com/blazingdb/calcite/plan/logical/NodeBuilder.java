b1a74e2db4adc2381daf7a2356556de4e730d720
gcca <cristhian@blazingdb.com>
2018-11-21 13:05:05 -0500

    [logical-plan] Add building exception support

--- blazingdb-calcite-core/src/main/java/com/blazingdb/calcite/plan/logical/NodeBuilder.java
+++ blazingdb-calcite-core/src/main/java/com/blazingdb/calcite/plan/logical/NodeBuilder.java
@@ -3,5 +3,10 @@ package com.blazingdb.calcite.plan.logical;
 import java.io.Serializable;
 
 public interface NodeBuilder extends Serializable {
-  Node build();
+  Node build() throws BuildingException;
+
+  public static class BuildingException extends Exception {
+    private static final long serialVersionUID = 8873783466414113910L;
+    public BuildingException(final String message) { super(message); }
+  }
 }
