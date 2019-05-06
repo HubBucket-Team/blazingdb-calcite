e8c2d85d36b74f76d887aa281ebf93fdf65814cc
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2019-05-06 17:35:05 -0500

    Dont mix calcite logic with unix sockets

--- blazingdb-calcite-application/src/main/java/com/blazingdb/calcite/application/UnixService.java
+++ blazingdb-calcite-application/src/main/java/com/blazingdb/calcite/application/UnixService.java
@@ -14,21 +14,6 @@ import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.apache.calcite.plan.RelOptUtil;
-
-import com.blazingdb.calcite.application.Chrono.Chronometer;
-import com.blazingdb.protocol.message.RequestMessage;
-import com.blazingdb.protocol.message.ResponseErrorMessage;
-import com.blazingdb.protocol.message.ResponseMessage;
-import com.blazingdb.protocol.message.calcite.DDLCreateTableRequestMessage;
-import com.blazingdb.protocol.message.calcite.DDLDropTableRequestMessage;
-import com.blazingdb.protocol.message.calcite.DDLResponseMessage;
-import com.blazingdb.protocol.message.calcite.DMLRequestMessage;
-import com.blazingdb.protocol.message.calcite.DMLResponseMessage;
-import com.blazingdb.protocol.util.ByteBufferUtil;
-
-import blazingdb.protocol.Status;
-import blazingdb.protocol.calcite.MessageType;
 import jnr.enxio.channels.NativeSelectorProvider;
 import jnr.unixsocket.UnixServerSocket;
 import jnr.unixsocket.UnixServerSocketChannel;
@@ -74,7 +59,7 @@ public class UnixService implements Runnable {
                 byte [] buffer = new byte[length];
                 int n = receiver.read(buffer);
                 if (n > 0) {
-                    ByteBuffer response = calciteService(ByteBuffer.wrap(buffer), argdataDirectory);
+                    ByteBuffer response = CalciteService.processRequest(ByteBuffer.wrap(buffer), argdataDirectory);
                     SocketChannelOutputStream sender = new SocketChannelOutputStream(channel);
                     byte [] responseBytes = response.array();
                     sender.write(responseBytes.length);
@@ -104,77 +89,6 @@ public class UnixService implements Runnable {
         address = new UnixSocketAddress(unixSocket);
         channel = UnixServerSocketChannel.open();
     }
-
-	public static ByteBuffer calciteService(ByteBuffer buffer, final String dataDirectory) {
-		Chronometer chronometer = Chronometer.makeStarted();
-
-		RequestMessage requestMessage = new RequestMessage(buffer);
-		if (requestMessage.getHeaderType() == MessageType.DML) {
-			DMLRequestMessage requestPayload = new DMLRequestMessage(requestMessage.getPayloadBuffer());
-			ResponseMessage response = null;
-			System.out.println("DML: " + requestPayload.getQuery());
-
-			try {
-				String logicalPlan = RelOptUtil.toString(ApplicationContext.getRelationalAlgebraGenerator(dataDirectory)
-						.getRelationalAlgebra(requestPayload.getQuery()));
-				DMLResponseMessage responsePayload = new DMLResponseMessage(logicalPlan,
-						chronometer.elapsed(MILLISECONDS));
-				response = new ResponseMessage(Status.Success, responsePayload.getBufferData());
-			} catch (SqlSyntaxException e) {
-				ResponseErrorMessage error = new ResponseErrorMessage(e.getMessage());
-				response = new ResponseMessage(Status.Error, error.getBufferData());
-			} catch (SqlValidationException e) {
-				ResponseErrorMessage error = new ResponseErrorMessage(e.getMessage());
-				response = new ResponseMessage(Status.Error, error.getBufferData());
-			} catch (Exception e) {
-				ResponseErrorMessage error = new ResponseErrorMessage(
-						"Improperly Formatted Query\n" + e.getStackTrace()[0]);
-				response = new ResponseMessage(Status.Error, error.getBufferData());
-			}
-			return response.getBufferData();
-		} else if (requestMessage.getHeaderType() == MessageType.DDL_CREATE_TABLE) {
-			DDLCreateTableRequestMessage message = new DDLCreateTableRequestMessage(requestMessage.getPayloadBuffer());
-			ResponseMessage response = null;
-			try {
-				ApplicationContext.getCatalogService(dataDirectory).createTable(message);
-				// I am unsure at this point if we have to update the schema or not but for safety I do it here
-				// need to see what hibernate moves around :)
-				ApplicationContext.updateContext(dataDirectory);
-				DDLResponseMessage responsePayload = new DDLResponseMessage(chronometer.elapsed(MILLISECONDS));
-				response = new ResponseMessage(Status.Success, responsePayload.getBufferData());
-			} catch (Exception e) {
-				ResponseErrorMessage error = new ResponseErrorMessage("Could not create table");
-				response = new ResponseMessage(Status.Error, error.getBufferData());
-
-			}
-			return response.getBufferData();
-		} else if (requestMessage.getHeaderType() == MessageType.DDL_DROP_TABLE) {
-			ResponseMessage response = null;
-
-			DDLDropTableRequestMessage message = new DDLDropTableRequestMessage(requestMessage.getPayloadBuffer());
-			try {
-				ApplicationContext.getCatalogService(dataDirectory).dropTable(message);
-				ApplicationContext.updateContext(dataDirectory);
-				DDLResponseMessage responsePayload = new DDLResponseMessage(chronometer.elapsed(MILLISECONDS));
-				response = new ResponseMessage(Status.Success, responsePayload.getBufferData());
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				ResponseErrorMessage error = new ResponseErrorMessage("Could not drop table");
-				response = new ResponseMessage(Status.Error, error.getBufferData());
-
-			}
-			return response.getBufferData();
-
-		} else {
-			ResponseMessage response = null;
-
-			ResponseErrorMessage error = new ResponseErrorMessage("unhandled request type");
-			response = new ResponseMessage(Status.Error, error.getBufferData());
-
-			return response.getBufferData();
-
-		}
-	}
 
     @Override
     public void run() {
